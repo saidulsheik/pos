f53b542c0eb2d4a5a2213af13449ce9ffa51b9d6
saidulidb27@gmail.com <saidulidb27@gmail.com>
2019-12-02 14:57:34 +0600

    Admin User Name Show.

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -174,7 +174,7 @@
         <img src="{{URL::to('dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+        <a href="#" class="d-block">{{Auth::user()->name}}</a>
         </div>
       </div>
 
